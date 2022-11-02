910833a46aa319bd51704abb9824e936d9986933
fardeen <70657851+AlphaMale6967@users.noreply.github.com>
2022-11-02 11:49:37 +0530

    added prime.js

---
+++ prime.js
@@ -0,0 +1,17 @@
+function prime(num){
+	let count = 0;
+	for(let i=0;i<=num;i++){
+		if(num%1==0){
+		count++;
+		}
+	}
+	if(count==2){
+		return true;
+	}
+	else{
+		return false;
+}
+
+
+let a = prime(33);
+console.log(a);
