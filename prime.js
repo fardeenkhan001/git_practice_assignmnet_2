34670f791c3c4218b2280d7b966044bbdd508052
fardeen <70657851+AlphaMale6967@users.noreply.github.com>
2022-11-02 13:01:10 +0530

    Committed changes

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
-function prime(num){
-	let count = 0;
-	for(let i=0;i<=num;i++){
-		if(num%1==0){
-		count++;
+function prime(number){
+	let counter = 0;
+	for(let i=0;i<=number;i++){
+		if(number%i==0){
+		counter++;
 		}
 	}
-	if(count==2){
+	if(counter==2){
 		return true;
 	}
 	else{
